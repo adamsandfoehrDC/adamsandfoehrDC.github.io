1731830adf751fc2415d20e820462f7956887365
AdamSandfoehrDC <adam.sandfoehr@dcmail.ca>
2026-07-28 14:21:01 -0400

    Completing returnRandomStoryString
    Added in required variables and embedded expressions to function, updated comments for notes.

--- assignment4/javascriptbasics/script.js
+++ assignment4/javascriptbasics/script.js
@@ -1,26 +1,32 @@
 // Complete variable definitions and random functions
-
 const customName = document.getElementById("custom-name");
+
 const generateBtn = document.querySelector(".generate");
+
 const story = document.querySelector(".story");
 
 function randomValueFromArray(array) {
-  const random = Math.floor(Math.random() * array.length);
-  return array[random];
+    const random = Math.floor(Math.random() * array.length);
+    
+    return array[random];
 }
 
 // Raw text strings
+// arrays made with []
 const characters = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
 
 const places = ["the soup kitchen", "Disneyland", "the White House"];
 
 const events = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and slithered away"];
 
-
 // Partial return random string function
-
 function returnRandomStoryString() {
-  // It was 94 Fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.
+    const randomCharacter = randomValueFromArray(characters);
+    const randomPlace = randomValueFromArray(places);
+    const randomEvent = randomValueFromArray(events);
+
+    // when inserting embeddded expressions you must use ` (~ key) to wrap the text, otherwise JS treats entire block as a string
+    let storyText = `It was 94 Fahrenheit outside, so ${randomCharacter} went for a walk. When they got to ${randomPlace}, they stared in horror for a few moments, then ${randomEvent}. Bob saw the whole thing, but was not surprised — ${randomCharacter} weighs 300 pounds, and it was a hot day.`
 
   return storyText;
 }
