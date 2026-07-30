d7c33740e8b91601be08c6e4d0ad7520e6d2c430
AdamSandfoehrDC <adam.sandfoehr@dcmail.ca>
2026-07-30 16:20:56 -0400

    Setup Loop & Thumbnail
    Setup for..of loop for images array, added tab & enter key function. laid groundwork for updateDisplayedImage function

--- assignment4/imagegallery/script.js
+++ assignment4/imagegallery/script.js
@@ -12,3 +12,35 @@ const images = [
     {filename: "pic4.jpg", alt: "Section of wall from a pharaoh's tomb."},
     {filename: "pic5.jpg", alt: "Large moth on a leaf."}
 ];
+
+// creating common URL variable to hold baseURL of image links
+const commonURL = "https://mdn.github.io/shared-assets/images/examples/learn/gallery/";
+
+// loop through images in array (for..of)
+for (const image of images) {
+    // create img element each instance of loop
+    const createdImage = document.createElement("img");
+
+    // updating image source & alt
+    // combine commonURL with filename of which image in images we are on
+    createdImage.src = `${commonURL}${image.filename}`;
+    // alt is called same as filename
+    createdImage.alt = image.alt;
+
+    // add tab index so user can focus with keyboard
+    // 0 is the first thing focused
+    createdImage.tabIndex = "0";
+
+    // adding image to thumbnail bar
+    thumbBar.appendChild(createdImage);
+
+    // make it so the image will run updateDisplayedImage when clicked
+    createdImage.addEventListener("click", updateDisplayedImage());
+
+    // allow <Enter> key press to make the focused image full sized
+    createdImage.addEventListener("keydown", function(EnterPress) {
+        if (EnterPress.code === "Enter") {
+            updateDisplayedImage(EnterPress)
+        }
+    })
+}
