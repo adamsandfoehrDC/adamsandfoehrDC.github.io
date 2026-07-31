5f734de2422b99018ae66bb3e1f69ec461f88a5d
AdamSandfoehrDC <adam.sandfoehr@dcmail.ca>
2026-07-30 22:09:33 -0400

    Added Function to visually create balls
    Created ball objects now visually appear on the screen when webpage is loaded.

--- assignment4/objectbuilding/script.js
+++ assignment4/objectbuilding/script.js
@@ -76,3 +76,18 @@ while (balls.length < 25) {
 
     balls.push(ball);
 }
+
+// drawing created ball objects
+function loop() {
+    ctx.fillStyle = "rgb(0 0 0 / 25%)";
+    ctx.fillRect(0, 0, width, height);
+
+    for (const ball of balls) {
+        ball.draw();
+        ball.update();
+    }
+
+    requestAnimationFrame(loop);
+}
+
+loop();
