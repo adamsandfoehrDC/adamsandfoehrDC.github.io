86e855d11e1a6ca0e8236ab0baf82040815920d1
AdamSandfoehrDC <adam.sandfoehr@dcmail.ca>
2026-07-30 22:12:44 -0400

    Added Collision function
    added function so balls will change rgb color upon colliding with other balls.

--- assignment4/objectbuilding/script.js
+++ assignment4/objectbuilding/script.js
@@ -58,6 +58,20 @@ class Ball {
         this.x += this.velX;
         this.y += this.velY;
     }
+
+    collisionDetect() {
+        for (const ball of balls) {
+            if (this !== ball) {
+                const dx = this.x - ball.x;
+                const dy = this.y - ball.y;
+                const distance = Math.sqrt(dx * dx + dy * dy);
+
+                if (distance < this.size + ball.size) {
+                    ball.color = this.color = randomRGB();
+                }
+            }
+        }
+    }
 }
 
 // adding balls to canvas
@@ -85,6 +99,7 @@ function loop() {
     for (const ball of balls) {
         ball.draw();
         ball.update();
+        ball.collisionDetect();
     }
 
     requestAnimationFrame(loop);
